214d3603e12b16bb91b70e74ac929dbd0101ae4a
BarkleyRhoat <barkleyrhoat@gmail.com>
2023-10-09 00:15:15 -0400

    arranged the var for confirms at the top of js so it appears as soon as you get on page

--- script.js
+++ script.js
@@ -1,45 +1,50 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", writePassword);
 
-// create the function here
-// var numOfCharacters = promt("how many characters do you want your password to be?");
-numOfCharacters = parseInt(numOfCharacters)
-
-while (passLength < 5 || passLengthength > 20){
-  passLength = promt("how many characters do you want your password to be")
-}
-  
-
-function generatePassword(){
-  return Math.floor(math.random()*max);
-
-}
 // prompt to get the number of characters in password
-var numOfCharacters = promt("how many characters do you want")
 
+var generateBtn = document.querySelector("#generate");
+var numOfCharacters = prompt("how many characters do you want your password to be?")
 
-// need 4 ifs with 4 confirmExpressionconditionals
-var upper = confirm("would you like to use uppercase letters?");
+var uppers = confirm("would you like to use uppercase letters?");
 var lowers = confirm("would you like to use lowercase letters?");
 var numbers = confirm("would you like to use numbers?");
 var symbols = confirm("would you like to use special characters?");
 
-while (!uppers && !lowers && !numbers && !symbols) {
-  alert("You must use atleast one character type");
+ if (!uppers && !lowers && !numbers && !symbols) {
+  alert ("You must use atleast one character type");
   uppers = confirm("would you like to use uppercase letters?");
   lowers = confirm("would you like to use lowercase letters?")
   numbers = confirm("would you like to use numbers?");
   symbols = confirm("would you like to use special characters?")
 }
-// processing...
 
-// return password
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var number = "0123456789";
+var symbol = "@#$%^&*()_+~|}{[]<>/-=";
 
-//   return "turtle"; // if turtle is a password
+var allCharacters = upperCase + lowerCase + number + symbol;
+
+function generatePassword(){
+  var password = ""
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
+
+  for (var numOfCharacters = 0; i < password.length; i++) {
+    password += allCharacters[Math.floor(Math.random() * allCharacters.length)];
+  }
+}
 
 
-// Write password to the #password input
+// numOfCharacters = parseInt(numOfCharacters)
+
+while (passLength < 5 || passLengthength > 20){
+  passLength = promt("how many characters do you want your password to be")
+}
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -47,7 +52,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 
 
 // Add event listener to generate button
