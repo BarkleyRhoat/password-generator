2514ea4efaaf5c6e2dc485eeeef2758a53509a3c
BarkleyRhoat <barkleyrhoat@gmail.com>
2023-10-11 21:53:00 -0400

    deleted spaces

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Assignment Code
-
-// prompt to get the number of characters in password
 
 var generateBtn = document.querySelector("#generate");
 var numOfCharacters = () => {
@@ -13,8 +10,6 @@ var numOfCharacters = () => {
   return numOfCharacters();
 }
 
-
-
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
@@ -26,9 +21,6 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-
-  
-
 }
 
 function generatePassword() {
